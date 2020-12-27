43637e8f6e41348e48d7a3868ccfa0c3c7bd4085
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2020-12-27 08:55:29 -0500

    added streams components and Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate'
+import StreamEdit from './streams/StreamEdit'
+import StreamDelete from './streams/StreamDelete'
+import StreamList from './streams/StreamList'
+import StreamShow from './streams/StreamShow'
+import Header from './Header';
+
+// 867139672249-vhtl7g75hjurbrgr5rv0qbvg5koginio.apps.googleusercontent.com
 
 const App = () => {
   return (
-    <div>
-      App
+    <div className="ui container"> 
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" component={StreamCreate} />
+          <Route path="/streams/edit" component={StreamEdit} />
+          <Route path="/streams/delete" component={StreamDelete} />
+          <Route path="/streams/show" component={StreamShow} />
+        </div>
+      </BrowserRouter>
     </div>
   )
 }
