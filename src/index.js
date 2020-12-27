a2a75ea3d5da83da3329697124bf393d6a1d623b
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2020-12-26 19:32:45 -0500

    created App component and installed react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import App from './components/App';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.querySelector('#root'));
+
+// App Challenges
+// Need to be able to navigate around to separate pages in our app
+// Need to allow a user to login/logout
+// Need to handle forms in Redux
+// Need to master CRUD operations in React/Redux
+// Need good error handling
