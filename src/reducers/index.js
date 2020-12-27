cdc4243e86eb176a02455cd978a2c4520ca3d86a
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2020-12-27 17:40:24 -0500

    created input forms with Redux-form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer 
+} from 'redux-form';
 import authReducer from './authReducer';
 
 export default combineReducers({
   // replaceMe: () => 5
-  auth: authReducer
+  auth: authReducer,
+  form: formReducer
 })
