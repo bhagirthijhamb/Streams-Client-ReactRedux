f46c49879eca8ccb58d23dbd63eeb54d0693f904
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2020-12-29 08:48:23 -0500

    created basic Modal for StreanDelete component

---
+++ src/components/Modal.js
@@ -0,0 +1,15 @@
+import react from 'react';
+import ReactDOM from 'react-dom';
+
+const Modal = props => {
+  return ReactDOM.createPortal(
+    <div className="ui dimmer modals visible active">
+      <div className="ui standard modal visible active">
+        Hare Krishan
+      </div>
+    </div>,
+    document.querySelector('#modal')
+  )
+}
+
+export default Modal;
