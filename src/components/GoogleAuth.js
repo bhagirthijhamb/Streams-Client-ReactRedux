f6e753ba8fd2922c9e6135cf24e5629e9984b6a6
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2021-01-11 16:42:14 -0500

    added some comments

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -4,6 +4,7 @@ import { signIn, signOut } from './../actions';
 
 class GoogleAuth extends React.Component {
   // state = { isSignedIn: null }
+
   componentDidMount(){
     // load additional library and pass a callback function as second arg that is called after this addtional module is loaded up
     window.gapi.load('client:auth2', () => {
@@ -13,16 +14,39 @@ class GoogleAuth extends React.Component {
         clientId: '867139672249-vhtl7g75hjurbrgr5rv0qbvg5koginio.apps.googleusercontent.com',
         scope: 'email'
       }).then(() => {
+        // this.auth instance can be used to sign the user in or get the users current authentcation status
         this.auth = window.gapi.auth2.getAuthInstance();
+        // we want to find out if the uaer is signed in and print our the authentication status on the screen
+        // but when this componentDidMount() gets called, the component has already been rendered to the screen
+        // now if we want to update what content this component shows, we need to somehow get the component to rerender
+        // So we make use of component level state here
+
+        // After we initialize our library and get access to the auth object
+        // we update the component level state of whether or not the user is currently signed in
+        // this will cause the component to rerender and we can print the authentication status on the screen
+
+        // right now we are calling setState()
         // this.setState({ isSignedIn: this.auth.isSignedIn.get() })
-        this.onAuthChange(this.auth.isSignedIn.get());
-        this.auth.isSignedIn.listen(this.onAuthChange);
+
+        // with Redux
+        // we should call props.signIn() or props.signOut() actions depending on if user is signed in
+        // dispatch an initial action when we finish initiating our libraray to indicate if the user is signed in
+        // reuse onAuthChnage() with this.auth.isSignedIn.get()
+        // this code runs at inititalization, when the library first boots up
+        this.onAuthChange(this.auth.isSignedIn.get()); // isSignedIn.get() gives true/false
+
+        // wait for authentication status to change at some time in future
+        this.auth.isSignedIn.listen(this.onAuthChange); // isSignedIn.listen() , if we pass a callback function to listen(), it will be invoked anytime users authentication status is chnaged
       })
     })
   }
   // Arrow functions because these are calback functions
-  onAuthChange = (isSignedIn) => {
-    // this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  // called anytime the users authentication status changes according to GAPI
+  // onAuthChange = () => {
+  onAuthChange = (isSignedIn) => { // when called frpm inside .listen(), it gets called with Boolean true/false (recieve as an argument)
+    // getting the value of isSignedIn from this.auth.isSignedIn.get()
+    // this.setState({ isSignedIn: this.auth.isSignedIn.get() }); // not required to reach to auth instance
+
     if(isSignedIn){
       this.props.signIn(this.auth.currentUser.get().getId());
     } else {
@@ -38,7 +62,8 @@ class GoogleAuth extends React.Component {
   renderAuthbutton(){
     // if(this.state.isSignedIn === null){
     if(this.props.isSignedIn === null){
-      return <div>I dont know if we are figned in</div>
+      // return <div>I dont know if we are signed in</div>
+      return null;
     // } else if(this.state.isSignedIn){
     } else if(this.props.isSignedIn){
       return (
